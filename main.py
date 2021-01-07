a2fd56b740dbd26a66fd24612185ab9f16301e69
vanikalapciev <31218797+vanikalapciev@users.noreply.github.com>
2021-01-07 13:38:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
-from graphics import *
 from random import *
 import random
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import animation
 
-def main():
-    win = GraphWin("Visualisation", 500, 500)
-    win.setBackground('gray')
-    numbers = random.sample(range(1, 500), 250)
-    i=1
-    j=0
-    while i < 500:
-        number = randint(0, 500)
-        win.create_line(i, 500, i, 500-numbers[j])
-        j += 1
-        i += 2
-    
-    win.getMouse()
-    win.close()
-main()
+
+setSize = 100
+i = 1
+
+numbers = random.sample(range(1, setSize), setSize-1)
+ind = np.arange(setSize-1)
+
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+ax.bar(ind,numbers)
+plt.show()
