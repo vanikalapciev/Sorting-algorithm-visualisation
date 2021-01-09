b26e8f50da8f30fbf78ec1a0e1ce729a18cdada2
vani <vani@192.168.1.102>
2021-01-09 09:32:09 +0100

    fixed animation performance issue

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
-from random import *
 import random
 import numpy as np
-import sorting
-import time
 from matplotlib import pyplot as plt
-from matplotlib import animation
 from sorting import *
-from matplotlib.animation import FuncAnimation
 
+#set sample size
+sampleSize = 100
 
-setSize = 50
-i=0
-
+#defines bar chart
 ax = plt.axes()
-plt.xlim(0,setSize) 
-plt.ylim(0,setSize)
-ind = np.arange(setSize-1)
+plt.xlim(0,sampleSize) 
+plt.ylim(0,sampleSize)
+ind = np.arange(sampleSize-1)
 
-numbers = random.sample(range(1, setSize), setSize-1)
+numbers = random.sample(range(1, sampleSize), sampleSize-1)
+sortArray = [numbers]
 
-for i in range(setSize):        
-     numbers = quick_sort(numbers, i) 
-     ax.bar(ind,numbers) 
-     plt.draw() 
-     plt.pause(0.001) 
-     if i < setSize-1:
-         ax.cla()
+#makes a array with each element with sorted run
+for i in range(sampleSize):
+    run = bubble_sort(numbers[:], i)
+    sortArray.append(run)
 
+#animates sorting
+for i in range(sampleSize+1):
+    ax.bar(ind,sortArray[i])
+    plt.draw() 
+    plt.pause(0.01)
+    if i < sampleSize:
+        ax.cla()
+        
 plt.show()
--- sorting.py
+++ sorting.py
@@ -1,6 +1,5 @@
 #Bubble sort
 def bubble_sort(list, noOfIter):
-    iterNum = 0
 # Swap the elements to arrange in order    for iter_num in range(len(list)-1,0,-1):
     for iter_num in range(noOfIter-1,0,-1):
         for idx in range(iter_num):
