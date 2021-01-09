0d61367a8d895b8e95a71fa8dbe2495b996b283f
vani <vani@192.168.1.102>
2021-01-09 15:21:05 +0100

    removed an animation bug

--- main.py
+++ main.py
@@ -11,20 +11,16 @@ ind = np.arange(sampleSize-1)
 
 #generates random number
 numbers = random.sample(range(1, sampleSize), sampleSize-1)
-sortArray = [numbers]
 
 #makes a array with each element with sorted run
 for i in range(sampleSize):
-    run = merge_sort(numbers[:], i)
-    sortArray.append(run)
-    print(run)
-
-#animates sorting
-for i in range(sampleSize+1):
-    plt.bar(ind,sortArray[i])
+    numbers = insertion_sort(numbers[:], i)
+    plt.bar(ind,numbers)
     plt.pause(0.1)
+    print(numbers)
     if i < sampleSize:
         plt.cla()
+
 
 #does not close window
 plt.show()
